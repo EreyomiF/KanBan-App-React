03e5c5bece2e124ce8b15b6043bd313583d0ccce
Ereyomi Funmilola <funmilolaereyomi@gmail.comgit config --global user.email funmilolaereyomi@gmail.com>
2024-01-30 15:29:48 -0800

    update

--- src/Components/TodoHeader.jsx
+++ src/Components/TodoHeader.jsx
@@ -6,7 +6,7 @@ import jackpot from '../assets/icons/jackpot.png';
 
 const GridWithBoxes = () => {
   return (
-    <div className="p-2 mb-4 rounded-lg mt-10">
+    <div className="p-2 mb-4 rounded-lg mt-8">
       <div className="grid grid-cols-3 gap-4 mt-4">
         <div className="flex flex-col items-center justify-center text-gray-900 hover:text-white border border-gray-800 hover:bg-gray-900 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mb-2 dark:border-gray-600 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-800">
           <ImageComponent src={chick} alt="Add Up" className="w-6 h-6 mb-2" />
